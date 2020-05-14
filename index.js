bbbfc14a845df6083d4023f825d5ed09ed6886a9
SMoore3773 <Smooresculpture@gmail.com>
2020-05-13 22:34:25 -0400

    initial commit

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
+require('dotenv').config();
 const server = require("./api/server.js");
 
-const port = 5000;
+
+const port = process.env.PORT || 5000;
+console.log(process.env.PORT)
+console.log('port:',port)
+
 server.listen(port, () => {
   console.log(`\n*** Server Running on http://localhost:${port} ***\n`);
 });
